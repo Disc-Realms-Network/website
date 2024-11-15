af83f183e565dbaa405686ea38bc04964d54ebd2
Lupancham <lupancham@gmail.com>
2024-11-15 12:59:03 -0500

    :3

--- js/main.js
+++ js/main.js
@@ -89,14 +89,14 @@ async function fetchServerStatus() {
 function resetCookies() {
     const cookies = document.cookie.split(";");
 
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i];
-            const eqPos = cookie.indexOf("=");
-            const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i];
+        const eqPos = cookie.indexOf("=");
+        const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
 
             // Set the cookie with an expired date
-            document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/";
-        }
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/";
+    }
 
     console.log("All cookies have been reset!");
 }
